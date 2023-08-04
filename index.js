05a7cf585c7d08d3523c6449d776da482a3ba310
Evandro Santos Afonso <evandrosantosafonso@live.com>
2023-08-04 10:19:41 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var n1 = Number(window.prompt('Digite um Número'))
+var n2 = Number(window.prompt('Digite outro Número'))
+var s = n1 + n2
+window.alert(`A soma entre ${n1} e ${n2} é ${s}`)
+
+
+
+
+
+var nome = window.prompt('Qual seu nome ?')
+window.alert (`Olá ${nome} vc ${nome} é Pica`)
